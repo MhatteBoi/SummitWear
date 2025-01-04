462b4917d84c6831239d6dda9a088490d7eb3547
Mhattias Roswall <Mhattias.r@gmail.com>
2025-01-04 15:40:39 +0100

    wokred on the herocomponent and home page

--- summitwear.client/src/app/features/home/home.module.ts
+++ summitwear.client/src/app/features/home/home.module.ts
@@ -1,18 +1,22 @@
+// Import necessary modules from Angular core and other libraries
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { HomeComponent } from './pages/home.component';
-import { RouterModule, Routes } from '@angular/router';
-import { HeroComponent } from './components/hero/hero.component';
+import { CommonModule } from '@angular/common'; // Provides common Angular directives like ngIf, ngFor
+import { HomeComponent } from './pages/home.component'; // The HomeComponent for this module
+import { RouterModule, Routes } from '@angular/router'; // To handle routing within this module
+import { HeroComponent } from './components/hero/hero.component'; // The HeroComponent, a child component used in the HomeComponent
+import { SharedModule } from '../../shared/shared.module'; // A shared module for common components, pipes, or services
 
+// Define the routes for this module
 const routes: Routes = [
-  { path: '', component: HomeComponent }
+  { path: '', component: HomeComponent } // Route that will render HomeComponent at the root path of this module
 ];
 
 @NgModule({
-  declarations: [HomeComponent, HeroComponent],
+  declarations: [HomeComponent, HeroComponent], // Declare the components to be used within this module
   imports: [
-    CommonModule,
-    RouterModule.forChild(routes)
+    CommonModule, // Include the CommonModule for common directives and pipes
+    RouterModule.forChild(routes), // Configure the router for this module using the defined routes
+    SharedModule // Import the SharedModule to make shared components/services available
   ]
 })
-export class HomeModule { }
+export class HomeModule { } // The HomeModule class that encapsulates everything for the home page of the app
--- summitwear.client/src/app/shared/shared.module.ts
+++ summitwear.client/src/app/shared/shared.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './navbar/navbar.component';
+import { ImageGridComponent } from './components/image-grid/image-grid.component';
 
 
 
 @NgModule({
-  declarations: [NavbarComponent], // Declare the NavbarComponent
+  declarations: [NavbarComponent, ImageGridComponent], // Declare the NavbarComponent and other components so other features can get them
   imports: [CommonModule],
-  exports: [NavbarComponent], // Export it so other modules can use it
+  exports: [NavbarComponent, ImageGridComponent], // Export it so other modules can use it
 })
 export class SharedModule { }
